637134640b80b2cfadaaa30d07969df70bad33dc
HMTD-Links <109021412+HMTD-Links@users.noreply.github.com>
2022-11-06 10:25:12 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -382,6 +382,26 @@ async def get_shortlink(link):
     if "http" == https:
         https = "https"
         link = link.replace("http", https)
+    url = f'https:/short.io/api'
+    params = {'api': pk_aSCPUp8rbxcf65aY,
+              'url': link,
+              }
+
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
+                data = await response.json()
+                if data["status"] == "success":
+                    return data['shortenedUrl']
+                else:
+                    logger.error(f"Error: {data['message']}")
+                    return f'https://{URL_SHORTENR_WEBSITE}/api?api={URL_SHORTNER_WEBSITE_API}&link={link}'
+
+async def get_shortlink(link):
+    https = link.split(":")[0]
+    if "http" == https:
+        https = "https"
+        link = link.replace("http", https)
     url = f'https://mdiskpro.xyz/api'
     params = {'api': URL_SHORTNER_WEBSITE_API,
               'url': link,
